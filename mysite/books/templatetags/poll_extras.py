f7934aa39f443f29b8f68362674f1643b28e3128
HarryMing <blues4@126.com>
2013-03-30 16:26:44 +0800

    Learn chapter 9-11.

--- mysite/books/templatetags/poll_extras.py
+++ mysite/books/templatetags/poll_extras.py
@@ -1,7 +1,77 @@
 from django import template
+import re
+import datetime
+from mysite.books.models import Publisher, Author, Book
 
 register = template.Library()
 
 @register.filter(name='cut')
 def cut(value, arg):
     return value.replace(arg, '')
+
+class CurrentTimeNode3(template.Node):
+    def __init__(self, format_string, var_name):
+        self.format_string = str(format_string)
+        self.var_name = var_name
+
+    def render(self, context):
+        now = datetime.datetime.now()
+        context[self.var_name] = now.strftime(self.format_string)
+        return ''
+        
+@register.tag(name="current_time")
+def do_current_time(parser, token):
+    # This version uses a regular expression to parse tag contents.
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        msg = '%r tag requires arguments' % token.contents[0]
+        raise template.TemplateSyntaxError(msg)
+
+    m = re.search(r'(.*?) as (\w+)', arg)
+    if m:
+        fmt, var_name = m.groups()
+    else:
+        msg = '%r tag had invalid arguments' % tag_name
+        raise template.TemplateSyntaxError(msg)
+
+    if not (fmt[0] == fmt[-1] and fmt[0] in ('"', "'")):
+        msg = "%r tag's argument should be in quotes" % tag_name
+        raise template.TemplateSyntaxError(msg)
+
+    return CurrentTimeNode3(fmt[1:-1], var_name)
+
+@register.tag(name="upper")
+def do_upper(parser, token):
+    nodelist = parser.parse(('endupper',))
+    parser.delete_first_token()
+    return UpperNode(nodelist)
+
+class UpperNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        output = self.nodelist.render(context)
+        return output.upper()
+
+#simple_tag
+@register.simple_tag
+def current_time_short(format_string):
+    try:
+        return datetime.datetime.now().strftime(str(format_string))
+    except UnicodeEncodeError:
+        return ''
+
+@register.inclusion_tag('book_snippet.html')
+def books_for_title(title):
+    books = Book.objects.filter(title=title)
+    return {'books': books}
+
+@register.inclusion_tag('link.html', takes_context=True)
+def jump_link(context):
+    return {
+        'link': 'http://www.baidu.com',#context['home_link'],
+        'title':'this is title', #context['home_title'],
+    }
